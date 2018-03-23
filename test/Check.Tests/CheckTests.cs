b5379bb19a77b39ce347bbd90cc973ee6b3007ec
Ronald Barnhart <professional.burgundy@gmail.com>
2018-03-22 21:05:46 -0400

    Added null checking, equality checking, and conditional checking.

---
+++ test/Check.Tests/CheckTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CheckNET.Core;
+
+namespace CheckNET.Tests
+{
+    [TestFixture]
+    public class CheckTests
+    {
+        [Test]
+        public void CheckIsNull()
+        {
+            string nullstring = null;
+
+            Assert.That(Check.IsNull(nullstring));
+        }
+
+        [Test]
+        public void CheckIsNotNull()
+        {
+            string emptystring = "";
+
+            Assert.That(Check.IsNotNull(emptystring));
+        }
+
+        [Test]
+        public void CheckIsEqual()
+        {
+            int? value = 100;
+            int? expectedValue = 100;
+
+            Assert.That(Check.IsEqual(value, expectedValue));
+        }
+
+        [Test]
+        public void CheckIsNotEqual()
+        {
+            int? value = 100;
+            int? expectedValue = 98;
+
+            Assert.That(Check.IsNotEqual(value, expectedValue));
+        }
+
+        [Test]
+        public void CheckMeetsCondition()
+        {
+            float? value = 100;
+            float expectedValue = 50;
+            float divisor = 2;
+
+            Assert.That(Check.MeetsCondition(value, startingValue => startingValue / divisor == expectedValue));
+        }
+    }
+}
