37573482fb2da85d03ea18c4bf6e6f446f6a1364
Ronald Barnhart <professional.burgundy@gmail.com>
2018-03-25 21:48:53 -0400

    Complete redesign of framework. Only support for strings and null checking objects.

---
+++ src/Check/Check.Core/Interactives/Check.cs
@@ -0,0 +1,36 @@
+﻿using CheckNET.Core.Assertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckNET.Core.Interactives
+{
+    public static class Check
+    {
+        public static void That<T>(Check<T> val)
+        {
+            var framework = GetFrameworkInUse(val);
+
+            if (framework == TestFramework.NUnit)
+            {
+                Assert.That(val.Result);
+            }
+        }
+
+        private static TestFramework GetFrameworkInUse<T>(Check<T> val)
+        {
+            var nunitAttribute = new StackFrame(2).GetMethod().CustomAttributes.Where(a => a.AttributeType.FullName.Contains("NUnit")).FirstOrDefault();
+
+            if (nunitAttribute != default(CustomAttributeData))
+            {
+                return TestFramework.NUnit;
+            }
+            else return TestFramework.NoFramework;
+        }
+    }
+}
---
+++ src/Check/Check.Core/Interactives/GenericCheck.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+using NUnit.Framework;
+
+/// <summary>
+/// We can do a couple of cool things with this.
+/// 
+/// I think we should make the extensions not extend Check<T> but rather the object type itself. 
+/// This allows the extensions to be used for assertions or just general value checking.
+/// 
+/// Namespace so that we have two NuGet packages essentially. One for assertions and one for checking.
+/// 
+/// Assertions should be handled within the Check.This() method. Make Check.This take in a Check<T>
+/// instead of T so we can assert on that value and keep it all clean.
+/// </summary>
+
+namespace CheckNET.Core.Interactives
+{
+    public class Check<T>
+    {
+        internal T Value { get; set; }
+        internal Type TypeOfT { get; set; }
+        internal bool Result { get; set; }
+        public T And => Value;
+
+        internal Check(T val)
+        {
+            Value = val;
+            TypeOfT = val.GetType();
+        }
+
+        internal Check(T val, bool res)
+        {
+            Value = val;
+            Result = res;
+        }
+    }
+}
