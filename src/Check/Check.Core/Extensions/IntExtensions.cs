6696fdcddc927f81ae85b238dd41d9229977380d
Ronald Barnhart <professional.burgundy@gmail.com>
2018-03-27 22:08:21 -0400

    Added int support and more verbose exceptions.

---
+++ src/Check/Check.Core/Extensions/IntExtensions.cs
@@ -0,0 +1,20 @@
+﻿using CheckNET.Core.Interactives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckNET.Core.Extensions
+{
+    public static class IntExtensions
+    {
+        public static Check<int> IsDivisbleBy(this int val, int divisor)
+        {
+            if (val % divisor > 0)
+                throw new ArgumentException($"Value {val} is not divisible by {divisor}.");
+
+            return new Check<int>(val, true);
+        }
+    }
+}
---
+++ src/Check/Check.Core/Extensions/NullableIntExtensions.cs
@@ -0,0 +1,23 @@
+﻿using CheckNET.Core.Interactives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckNET.Core.Extensions
+{
+    public static class NullableIntExtensions
+    {
+        public static Check<int?> IsDivisbleBy(this int? val, int? divisor)
+        {
+            var checkedValue = val ?? throw new ArgumentNullException("Cannot evaluate IsDivisibleBy() with a null int value.-");
+            var checkedDivisor = divisor ?? throw new ArgumentNullException("Cannot evaluate IsDivisibleBy() with a null divisor.");
+
+            if (val % divisor > 0)
+                throw new ArgumentException($"Value {val} is not divisible by {divisor}.");
+
+            return new Check<int?>(val, true);
+        }
+    }
+}
