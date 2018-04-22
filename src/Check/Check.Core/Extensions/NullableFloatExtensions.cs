211d493965f99fddd55819bd64a1cba73b189e3e
Ronald Barnhart <professional.burgundy@gmail.com>
2018-04-22 13:04:20 -0400

    Added Tests for floats

---
+++ src/Check/Check.Core/Extensions/NullableFloatExtensions.cs
@@ -0,0 +1,66 @@
+﻿using CheckNET.Core.Interactives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckNET.Core.Extensions
+{
+    public static class NullableFloatExtensions
+    {
+        public static Check<float?> IsDivisibleBy(this float? value, float divisor)
+        {
+            value = value ?? throw new ArgumentNullException(nameof(value));
+
+            if (value % divisor > 0)
+                throw new ArgumentException($"Value {value} is not divisible by {divisor}.");
+
+            return new Check<float?>(value, true);
+        }
+
+        public static Check<float?> IsDivisibleBy(this float? value, int divisor)
+        {
+            value = value ?? throw new ArgumentNullException(nameof(value));
+
+            if (value % divisor > 0)
+                throw new ArgumentException($"Value {value} is not divisible by {divisor}.");
+
+            return new Check<float?>(value, true);
+        }
+
+        public static Check<float?> IsDivisibleBy(this float? value, float? divisor)
+        {
+            value = value ?? throw new ArgumentNullException(nameof(value));
+            divisor = divisor ?? throw new ArgumentNullException(nameof(divisor));
+
+            if (value % divisor > 0)
+                throw new ArgumentException($"Value {value} is not divisible by {divisor}.");
+
+            return new Check<float?>(value, true);
+        }
+
+        public static Check<float?> IsDivisibleBy(this float? value, int? divisor)
+        {
+            value = value ?? throw new ArgumentNullException(nameof(value));
+            divisor = divisor ?? throw new ArgumentNullException(nameof(divisor));
+
+            if (value % divisor > 0)
+                throw new ArgumentException($"Value {value} is not divisible by {divisor}.");
+
+            return new Check<float?>(value, true);
+        }
+
+        public static Check<float?> StartsWith(this float? val, int digit)
+        {
+            val = val ?? throw new ArgumentNullException(nameof(val));
+
+            char[] digits = val.ToString().ToCharArray();
+
+            if (!Int32.Parse(digits[0].ToString()).Equals(digit))
+                throw new ArgumentException();
+
+            return new Check<float?>(val, true);
+        }
+    }
+}
---
+++ test/Check.Tests/NUnit/FloatTests.cs
@@ -0,0 +1,83 @@
+﻿using CheckNET.Core.Extensions;
+using CheckNET.Core.Interactives;
+using NUnit.Framework;
+
+namespace CheckNET.Tests.NUnit
+{
+    [TestFixture]
+    public class FloatTests
+    {
+        [Test]
+        [TestCase(10f, 2)]
+        [TestCase(20f, 10)]
+        [TestCase(100f, 5)]
+        public void Check_IsDivisibleBy_Int(float value, int divisor)
+        {
+            Check.That(value.IsDivisibleBy(divisor));
+        }
+
+        [Test]
+        [TestCase(10f, 2f)]
+        [TestCase(20f, 10f)]
+        [TestCase(100f, 5f)]
+        public void Check_IsDivisibleBy_Float(float value, float divisor)
+        {
+            Check.That(value.IsDivisibleBy(divisor));
+        }
+
+        [Test]
+        [TestCase(10f, 2)]
+        [TestCase(20f, 10)]
+        [TestCase(100f, 5)]
+        public void Check_IsDivisibleBy_IntNullable(float value, int? divisor)
+        {
+            Check.That(value.IsDivisibleBy(divisor));
+        }
+
+        [Test]
+        [TestCase(10f, 2f)]
+        [TestCase(20f, 10f)]
+        [TestCase(100f, 5f)]
+        public void Check_IsDivisibleBy_FloatNullable(float value, float? divisor)
+        {
+            Check.That(value.IsDivisibleBy(divisor));
+        }
+
+        [Test]
+        [TestCase(10f, 2)]
+        [TestCase(20f, 10)]
+        [TestCase(100f, 5)]
+        public void Check_NullableIsDivisibleBy_IntNullable(float? value, int? divisor)
+        {
+            Check.That(value.IsDivisibleBy(divisor));
+        }
+
+        [Test]
+        [TestCase(10f, 2f)]
+        [TestCase(20f, 10f)]
+        [TestCase(100f, 5f)]
+        public void Check_NullableIsDivisibleBy_FloatNullable(float? value, float? divisor)
+        {
+            Check.That(value.IsDivisibleBy(divisor));
+        }
+
+        [Test]
+        [TestCase(10f, 2)]
+        [TestCase(20f, 10)]
+        [TestCase(100f, 5)]
+        public void Check_NullableIsDivisibleBy_Int(float? value, int divisor)
+        {
+            Check.That(value.IsDivisibleBy(divisor));
+        }
+
+        [Test]
+        [TestCase(10f, 2f)]
+        [TestCase(20f, 10f)]
+        [TestCase(100f, 5f)]
+        public void Check_NullableIsDivisibleBy_Float(float? value, float divisor)
+        {
+            Check.That(value.IsDivisibleBy(divisor));
+        }
+
+    }
+}
